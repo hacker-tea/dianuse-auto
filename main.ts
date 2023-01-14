6b9a517a8338a405a5eb76069a866935e68f8812
hacker-tea <121435491+hacker-tea@users.noreply.github.com>
2023-01-14 12:54:20 +0200

    move forward

--- main.ts
+++ main.ts
@@ -1,2 +1,8 @@
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 19)
-basic.pause(200)
+basic.forever(function () {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 19)
+    basic.pause(500)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    maqueen.motorStop(maqueen.Motors.M1)
+    basic.pause(500)
+})
